5ac18c472f85fc64e3f92d7c48ed7443aef097db
Bruno Benčević <93935267+bruno-bencevic@users.noreply.github.com>
2023-02-12 16:52:58 +0100

    Initial Commit

---
+++ TVMazeApp/TVMazeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeAppApp.swift
+//  TVMazeApp
+//
+//  Created by Bruno Bencevic on 12.02.2023..
+//
+
+import SwiftUI
+
+@main
+struct TVMazeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
